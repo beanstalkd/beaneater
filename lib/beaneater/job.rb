ea28dff2a68b114c70ab960a26ebdee0643bc22e
Nathan Esquenazi <nesquena@gmail.com>
2012-10-24 01:12:53 -0700

    [job] Move class methods out to a separate class

--- lib/beaneater/job.rb
+++ lib/beaneater/job.rb
@@ -1,31 +1,30 @@
 module Beaneater
-  class Job < Command
-    # Class Methods
-    class << self
-      # @beaneater_connection.jobs.register('tube2', :retry_on => [Timeout::Error]) do |job|
-      #  process_one(job)
-      # end
-      def register(tube_name, options={})
+  class Jobs < Command
+    # @beaneater_connection.jobs.register('tube2', :retry_on => [Timeout::Error]) do |job|
+    #  process_one(job)
+    # end
+    def register(tube_name, options={})
 
-      end
-
-      # @beaneater_connection.jobs.process # all described tubes
-      # watch all described tubes
-      # ignore all other tubes
-      # loop do
-      # reserve
-      # invoke describe block based on tube
-      # success => delete, fail => bury
-      def process!
-
-      end
-
-      def find(id)
-
-      end
-      alias_method :peek, :find
     end
 
+    # @beaneater_connection.jobs.process # all described tubes
+    # watch all described tubes
+    # ignore all other tubes
+    # loop do
+    # reserve
+    # invoke describe block based on tube
+    # success => delete, fail => bury
+    def process!
+
+    end
+
+    def find(id)
+
+    end
+    alias_method :peek, :find
+  end # Jobs
+
+  class Job < Command
     # Instance Methods
     # @beaneater_tube.put "data", :priority => 1000, :ttr => 10, :delay => 5
     def put(data, options={})
@@ -44,6 +43,5 @@ module Beaneater
     def stats
 
     end
-
-  end
-end
+  end # Job
+end # Beaneater
