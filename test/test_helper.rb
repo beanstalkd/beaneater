dad8b32957226ec8bd5a30c3e54b24c266f3c67f
Nico Taing <nico.taing@gmail.com>
2012-10-26 21:15:06 -0700

    [test_helper] add helper to cleanup tubes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,3 +7,27 @@ require 'fakeweb'
 require 'mocha'
 
 FakeWeb.allow_net_connect = false
+
+class MiniTest::Unit::TestCase
+  def cleanup_tubes!(tubes)
+    tubes.each do |t|
+      begin
+        Timeout.timeout(1) do
+          @pool.tubes.watch!(t)
+          tube = @pool.tubes.find(t)
+          if tube.peek(:delayed)
+            while delayed = tube.peek(:delayed) do
+              delayed.delete
+            end
+          else
+            @pool.tubes.reserve do |job|
+              job.delete
+            end
+          end
+        end
+      rescue Timeout::Error
+        # nothing
+      end
+    end
+  end
+end
