b77622a772419a64bef02c6f55302a4a42c03325
Nico Taing <nico.taing@gmail.com>
2012-10-26 21:15:22 -0700

    [tube] add test for #peek

--- test/tube_test.rb
+++ test/tube_test.rb
@@ -6,7 +6,7 @@ describe Beaneater::Tube do
   describe "for #put" do
     before do
       @pool = Beaneater::Pool.new(['localhost'])
-      @tube = Beaneater::Tube.new(@pool, 'foo')
+      @tube = Beaneater::Tube.new(@pool, 'baz')
     end
 
     it "should insert a job" do
@@ -18,25 +18,31 @@ describe Beaneater::Tube do
       @tube.put "delayed", :delay => 1
       assert_equal "delayed", @tube.peek(:delayed).body
     end
-
-    after do
-      begin
-        Timeout.timeout(1) do
-          @pool.tubes.watch!('foo')
-          tube = @pool.tubes.find('foo')
-          if tube.peek(:delayed)
-            while delayed = tube.peek(:delayed) do
-              delayed.delete
-            end
-          else
-            @pool.tubes.reserve('foo') do |job|
-              job.delete
-            end
-          end
-        end
-      rescue Timeout::Error
-        # nothing
-      end
-    end
   end #find
+
+  describe "for #peek" do
+    before do
+      @pool = Beaneater::Pool.new(['localhost'])
+      @tube = Beaneater::Tube.new(@pool, 'baz')
+      @time = Time.now.to_i
+    end
+
+    it "should peek delayed" do
+      @tube.put "foo #{@time}", :delay => 2
+      assert_equal "foo #{@time}", @tube.peek(:delayed).body
+    end
+
+    it "should peek ready" do
+      @tube.put "foo #{@time}", :delay => 0
+      assert_equal "foo #{@time}", @tube.peek(:ready).body
+    end
+
+    # it "should peek buried" do
+    #   TODO add bury test
+    # end
+  end
+
+  after do
+    cleanup_tubes!(['baz']) if @tube.peek(:ready) || @tube.peek(:delayed)
+  end
 end # Beaneater::Tubes
