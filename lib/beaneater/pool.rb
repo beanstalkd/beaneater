a9ea382125809334aee899ef9d877bb49dd31fb5
Nathan Esquenazi <nesquena@gmail.com>
2012-10-26 22:40:03 -0700

    [pool] Convert hosts to array explicitly, use 'choice' on 1.8.7

--- lib/beaneater/pool.rb
+++ lib/beaneater/pool.rb
@@ -3,7 +3,7 @@ module Beaneater
     attr_reader :connections
 
     def initialize(hosts)
-      @connections = hosts.map { |h| Connection.new(h) }
+      @connections = Array(hosts).map { |h| Connection.new(h) }
     end
 
     def stats
@@ -16,25 +16,25 @@ module Beaneater
 
     # transmit_to_all("stats", :match => /\n/)
     def transmit_to_all(command, options={}, &block)
-      connections.map do |c|
-        c.transmit(command, options, &block)
+      connections.map do |conn|
+        conn.transmit(command, options, &block)
       end
     end
 
     # transmit_to_rand("stats", :match => /\n/)
     def transmit_to_rand(command, options={}, &block)
-      conn = connections.sample
+      conn = connections.respond_to?(:sample) ? connections.sample : connections.choice
       conn.transmit(command, options, &block)
     end
 
     # transmit_until_res('peek-ready', :status => "FOUND", &block)
     def transmit_until_res(command, options={}, &block)
       status_expected  = options.delete(:status)
-      connections.each do |c|
-        res = c.transmit(command, options, &block)
+      connections.each do |conn|
+        res = conn.transmit(command, options, &block)
         return res if res[:status] == status_expected
       end
       nil
     end
-  end
-end
+  end # Pool
+end # Beaneater
