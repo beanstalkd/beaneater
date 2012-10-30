16ce60f50f2499feace4eba216660d5d503290eb
Nico Taing <nico.taing@gmail.com>
2012-10-30 01:21:39 -0700

    [jobs] add #process! and register!

--- lib/beaneater/job/collection.rb
+++ lib/beaneater/job/collection.rb
@@ -1,21 +1,42 @@
 module Beaneater
   class Jobs < PoolCommand
+    class AbortProcessException < StandardError; end
+
+    attr_reader :process_jobs
+
+    MAX_RETRIES = 3
+    RELEASE_DELAY = 1
+
     # @beaneater_connection.jobs.register('tube2', :retry_on => [Timeout::Error]) do |job|
     #  process_one(job)
     # end
-    def register(tube_name, options={})
-
+    def register(tube_name, options={}, &block)
+      @process_jobs ||= {}
+      max_retries = options[:max_retries] || MAX_RETRIES
+      retry_on = Array(options[:retry_on])
+      @process_jobs[tube_name.to_s] = { :block => block, :retry_on => retry_on, :max_retries => max_retries }
     end
 
     # @beaneater_connection.jobs.process # all described tubes
-    # watch all described tubes
-    # ignore all other tubes
-    # loop do
-    # reserve
-    # invoke describe block based on tube
-    # success => delete, fail => bury
-    def process!
-
+    # release_delay
+    def process!(options={})
+      release_delay = options.delete(:release_delay) || RELEASE_DELAY
+      tubes.watch!(*process_jobs.keys)
+      loop do
+        job = tubes.reserve
+        processor = process_jobs[job.stats.tube]
+        begin
+          processor[:block].call job
+          job.delete
+        rescue AbortProcessException
+          job.bury
+          break
+        rescue *processor[:retry_on]
+          job.stats.releases >= processor[:max_retries] ? job.bury : job.release(:delay => release_delay)
+        rescue
+          job.bury
+        end
+      end
     end
 
     # @beaneater_connection.jobs.find(123)
--- test/jobs_test.rb
+++ test/jobs_test.rb
@@ -25,7 +25,75 @@ describe Beaneater::Jobs do
     end
   end # find
 
+  describe "for #register!" do
+    before do
+      $foo = 0
+      @jobs.register('tube', :retry_on => [Timeout::Error]) do |job|
+        $foo += 1
+      end
+    end
+
+    it "should store processor" do
+      assert_equal 'tube', @jobs.process_jobs.keys.first
+      assert_equal [Timeout::Error], @jobs.process_jobs.values.first[:retry_on]
+    end
+
+    it "should store block for 'tube'" do
+      @jobs.process_jobs['tube'][:block].call nil
+      assert_equal 1, $foo
+    end
+  end # register!
+
+  describe "for process!" do
+    before do
+      $foo = []
+
+      @jobs.register('tube_success', :retry_on => [Timeout::Error]) do |job|
+        # p job.body
+        $foo << job.body
+        raise Beaneater::Jobs::AbortProcessException if job.body =~ /abort/
+      end
+
+      @jobs.register('tube_release', :retry_on => [Timeout::Error], :max_retries => 2) do |job|
+        $foo << job.body
+        raise Timeout::Error
+      end
+
+      @jobs.register('tube_buried') do |job|
+        $foo << job.body
+        raise RuntimeError
+      end
+
+      cleanup_tubes!(['tube_success', 'tube_release', 'tube_buried'])
+
+      @pool.tubes.find('tube_success').put("success abort", :pri => 2**31 + 1)
+      @pool.tubes.find('tube_success').put("success 2", :pri => 1)
+      @pool.tubes.find('tube_release').put("released")
+      @pool.tubes.find('tube_buried').put("buried")
+
+      @jobs.process!(:release_delay => 0)
+    end
+
+    it "should process all jobs" do
+      assert_equal ['success 2', 'released', 'released', 'released', 'buried', 'success abort'], $foo
+    end
+
+    it "should clear successful_jobs" do
+      assert_equal 0, @pool.tubes.find('tube_success').stats.current_jobs_ready
+      assert_equal 1, @pool.tubes.find('tube_success').stats.current_jobs_buried
+      assert_equal 0, @pool.tubes.find('tube_success').stats.current_jobs_reserved
+    end
+
+    it "should retry release jobs 2 times" do
+      assert_equal 2, @pool.tubes.find('tube_release').peek(:buried).stats.releases
+    end
+
+    it "should bury unexpected exception" do
+      assert_equal 1, @pool.tubes.find('tube_buried').stats.current_jobs_buried
+    end
+  end
+
   after do
-    cleanup_tubes!(['baz'])
+    cleanup_tubes!(['baz', 'tube_success', 'tube_release', 'tube_buried'])
   end
 end
