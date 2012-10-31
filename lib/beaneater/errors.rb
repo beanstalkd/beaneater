b9087f315fab5b53e395cf3c5ab5e43ba65fbb66
Nico Taing <nico.taing@gmail.com>
2012-10-30 21:54:23 -0700

    [errors] add missing exceptions and remove unused exceptions

--- lib/beaneater/errors.rb
+++ lib/beaneater/errors.rb
@@ -1,16 +1,15 @@
 module Beaneater
   class NotConnected < RuntimeError; end
-  class WaitingForJobError < RuntimeError; end
   class InvalidTubeName < RuntimeError; end
 
   class UnexpectedResponse < RuntimeError
     ERROR_STATES = %w(OUT_OF_MEMORY INTERNAL_ERROR
       BAD_FORMAT UNKNOWN_COMMAND JOB_TOO_BIG DRAINING
-      TIMED_OUT DEADLINE_SOON NOT_FOUND)
+      TIMED_OUT DEADLINE_SOON NOT_FOUND NOT_IGNORED EXPECTED_CRLF)
 
     attr_reader :status, :cmd
 
-    def initialize(status=nil, cmd=nil)
+    def initialize(status, cmd)
       @status, @cmd = status, cmd
       super("Response failed with: #{status}")
     end
@@ -35,4 +34,5 @@ module Beaneater
   class ExpectedCRLFError < UnexpectedResponse; end
   class JobTooBigError < UnexpectedResponse; end
   class TimedOutError < UnexpectedResponse; end
+  class NotIgnoredError < UnexpectedResponse; end
 end
