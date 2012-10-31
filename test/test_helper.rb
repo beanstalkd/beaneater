fd0b7252c99557b33b8fc2cfbec57505d571fcb8
Nico Taing <nico.taing@gmail.com>
2012-10-30 19:50:10 -0700

    [test_helper] cleaning tubes even if exceptions happen

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,14 +15,18 @@ class MiniTest::Unit::TestCase
   def cleanup_tubes!(tubes, bp=nil)
     bp ||= @pool
     tubes.each do |name|
-      bp.tubes.watch!(name)
-      tube = bp.tubes.find(name)
-      %w(delayed buried ready).each do |state|
-        while job = tube.peek(state.to_sym)
-          job.delete
+      begin
+        bp.tubes.watch!(name)
+        tube = bp.tubes.find(name)
+        %w(delayed buried ready).each do |state|
+          while job = tube.peek(state.to_sym)
+            job.delete
+          end
         end
+        bp.tubes.ignore!(name)
+      rescue Beaneater::UnexpectedResponse
+        next
       end
-      bp.tubes.ignore!(name)
     end
   end
 end
