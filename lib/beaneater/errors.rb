28d804764a743db27ee0ee3e9638e36a54f2f799
Nathan Esquenazi <nesquena@gmail.com>
2012-11-01 23:17:10 -0700

    [errors] Document purpose of each error type

--- lib/beaneater/errors.rb
+++ lib/beaneater/errors.rb
@@ -1,11 +1,14 @@
 module Beaneater
+  # Raises when the beanstalkd instance cannot be accessed
   class NotConnected < RuntimeError; end
+  # Raises when the tube name specified is invalid.
   class InvalidTubeName < RuntimeError; end
+  # Raises when a job has not been reserved properly.
   class JobNotReserved < RuntimeError; end
 
+  # Abstract class for errors that occur when a command does not complete successfully.
   class UnexpectedResponse < RuntimeError
-
-    # Different error states from beanstalkd server
+    # Set of status states that are considered errors
     ERROR_STATES = %w(OUT_OF_MEMORY INTERNAL_ERROR
       BAD_FORMAT UNKNOWN_COMMAND JOB_TOO_BIG DRAINING
       TIMED_OUT DEADLINE_SOON NOT_FOUND NOT_IGNORED EXPECTED_CRLF)
@@ -23,6 +26,7 @@ module Beaneater
     #
     # @example
     #   Beaneater::UnexpectedResponse.new(NotFoundError, 'bury 123')
+    #
     def initialize(status, cmd)
       @status, @cmd = status, cmd
       super("Response failed with: #{status}")
@@ -35,6 +39,7 @@ module Beaneater
     #
     # @example
     #   Beaneater::UnexpectedResponse.new('NOT_FOUND', 'bury 123')
+    #
     def self.from_status(status, cmd)
       error_klazz_name = status.split('_').map { |w| w.capitalize }.join
       error_klazz_name << "Error" unless error_klazz_name =~ /Error$/
@@ -43,15 +48,26 @@ module Beaneater
     end
   end
 
+  # Raises when the beanstalkd instance runs out of memory
   class OutOfMemoryError < UnexpectedResponse; end
+  # Raises when the beanstalkd instance is draining and new jobs cannot be inserted
   class DrainingError < UnexpectedResponse; end
+  # Raises when the job or tube cannot be found
   class NotFoundError < UnexpectedResponse; end
+  # Raises when the job reserved is going to be released within a second.
   class DeadlineSoonError < UnexpectedResponse; end
+  # Raises when a beanstalkd has an internal error.
   class InternalError < UnexpectedResponse; end
+  # Raises when a command was not properly formatted.
   class BadFormatError < UnexpectedResponse; end
+  # Raises when a command was sent that is unknown.
   class UnknownCommandError < UnexpectedResponse; end
+  # Raises when command does not have proper CRLF suffix.
   class ExpectedCRLFError < UnexpectedResponse; end
+  # Raises when the body of a job was too large.
   class JobTooBigError < UnexpectedResponse; end
+  # Raises when a job was attempted to be reserved but the timeout occured.
   class TimedOutError < UnexpectedResponse; end
+  # Raises when a tube could not be ignored because it is the last watched tube.
   class NotIgnoredError < UnexpectedResponse; end
 end
