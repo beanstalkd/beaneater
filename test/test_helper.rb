8c3167a48654663349b29bd6e06287a38ddfd3a7
Nico Taing <nico.taing@gmail.com>
2013-01-22 21:22:19 -0800

    [test/helper] require 'mocha'

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ require 'minitest/autorun'
 $:.unshift File.expand_path("../../lib")
 require 'beaneater'
 require 'fakeweb'
-require 'mocha/setup'
+require 'mocha'
 require 'json'
 
 FakeWeb.allow_net_connect = false
