21dcaf23ed38491b41c19516fcee6c41fbd97c26
Andreas Loupasakis <alup@aloop.org>
2015-02-24 00:36:43 +0200

    Fix test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,11 +12,11 @@ require 'json'
 class MiniTest::Unit::TestCase
 
   # Cleans up all jobs from tubes
-  # cleanup_tubes!(['foo'], @bp)
-  def cleanup_tubes!(tubes, bp=nil)
-    bp ||= @pool
+  # cleanup_tubes!(['foo'], @beanstalk)
+  def cleanup_tubes!(tubes, client=nil)
+    client ||= @beanstalk
     tubes.each do |name|
-      bp.tubes.find(name).clear
+      client.tubes.find(name).clear
     end
   end
 end
